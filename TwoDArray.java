55206f88cded03d19fabeb3418127205070f30f9
Jeff Blankenship <JeffBship@aol.com>
2021-10-05 07:45:21 -0400

    restore to original

--- TwoDArray.java
+++ TwoDArray.java
@@ -1,41 +1,41 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+//Jeff Blankenship
+//CS-151
+//2d loop iteration example
 
-public class Solution {
+//this code creates a 2d array of ints and assigns
+//random values to the elements
 
+import java.util.Random;
 
+public class TwoDArray{
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) 
-    {
-        int[][] arr = new int[6][6];
-        List<Integer> values = new ArrayList<>();
-
-        for (int i = 0; i < 6; i++) 
-        {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < 6; j++) 
-            {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
-                if(i - 2 >= 0 && j - 2 >= 0)
-                {
-                int firstRow = (arr[i][j])+(arr[i][j-1])+(arr[i][j-2]);
-                int secondRow = arr[i-1][j-1];
-                int thirdRow = arr[i-2][j]+arr[i-2][j-1]+arr[i-2][j-2];
-                    values.add(firstRow+secondRow+thirdRow);
-                }
-            }
-        }
-        System.out.println(Collections.max(values));
-        scanner.close();
+  public static void main (String[] args){
+    
+    //Random object for generating random numbers
+    Random random = new Random();
+    
+    int[][] intArray = new int[5][3];
+    
+    //indexA through outer layer
+    for (int indexA=0; indexA<intArray.length; indexA++){
+      //indexB through inner layer
+      for (int indexB=0; indexB<intArray[indexA].length; indexB++){
+        //assign random int 1-100 inclusive
+        intArray[indexA][indexB] = random.nextInt(100) + 1;
+      }      
     }
+    
+    //print the array using do loops
+    int indexA = 0;
+    do{//indexA through outer layer
+      int indexB = 0;
+      do {//indexB through inner layer
+        System.out.printf("%5d",intArray[indexA][indexB]);
+        indexB++;
+      } while (indexB<intArray[indexA].length);
+      //new line at the end of each row
+      System.out.println();
+      indexA++;
+    } while (indexA<intArray.length);
+  }
 }
